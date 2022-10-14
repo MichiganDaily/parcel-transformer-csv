f3be19459a50a22aa736be3a373f879f352a03b8
erxclau <ericlau41802@gmail.com>
2022-10-14 17:31:32 -0400

    Parse autoType query parameter

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ module.exports = new Transformer({
   async transform({ asset }) {
     const code = await asset.getCode();
     asset.type = "js";
+    let parseFunction = `csvParse(\`${code}\`, autoType)`;
+    if (
+      asset.query.has("autoType") &&
+      asset.query.get("autoType") === "false"
+    ) {
+      parseFunction = `csvParse(\`${code}\`)`;
+    }
     asset.setCode(`
       import { csvParse, autoType } from "d3-dsv";
-      export default csvParse(\`${code}\`, autoType);
+      export default ${parseFunction};
     `);
     return [asset];
   },
