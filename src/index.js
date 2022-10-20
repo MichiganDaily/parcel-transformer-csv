fa15cf26996b3a9b45828d3c599d9e4a1cb292fe
erxclau <ericlau41802@gmail.com>
2022-10-20 17:08:27 -0400

    Refactor parse function retrieval

--- src/index.js
+++ src/index.js
@@ -1,36 +1,45 @@
 const { Transformer } = require("@parcel/plugin");
 
+const csv = "csvParse";
+const tsv = "tsvParse";
+const dsv = "dsvFormat";
+const auto = "autoType";
+
+const getParser = (query, type) => {
+  let parser = csv;
+  if (query.has("delimiter")) {
+    const delimiter = query.get("delimiter");
+    if (delimiter === ",") {
+      parser = csv;
+    } else if (delimiter === "\t") {
+      parser = tsv;
+    } else {
+      parser = `${dsv}("${delimiter}").parse`;
+    }
+  } else if (type === "csv") {
+    parser = csv;
+  } else if (type === "tsv") {
+    parser = tsv;
+  }
+
+  return parser;
+};
+
 module.exports = new Transformer({
   async transform({ asset }) {
     const code = await asset.getCode();
     const q = asset.query;
 
-    let p = "csvParse";
-    if (q.has("delimiter")) {
-      const delimiter = q.get("delimiter");
-      if (delimiter === ",") {
-        p = "csvParse";
-      } else if (delimiter === "\t") {
-        p = "tsvParse";
-      } else {
-        p = `dsvFormat("${delimiter}").parse`;
-      }
-    } else if (asset.type === "csv") {
-      p = "csvParse";
-    } else if (asset.type === "tsv") {
-      p = "tsvParse";
-    }
+    const shouldAutoType = !(q.has(auto) && q.get(auto) === "false");
+    const autoType = shouldAutoType ? `, ${auto}` : "";
 
-    let data = `${p}(\`${code}\`, autoType)`;
-    if (q.has("autoType") && q.get("autoType") === "false") {
-      data = `${p}(\`${code}\`)`;
-    }
-
-    const parser = p === "csvParse" || p === "tsvParse" ? p : "dsvFormat";
+    const p = getParser(q, asset.type);
+    const data = `${p}(\`${code}\` ${autoType})`;
+    const parser = p === csv || p === tsv ? p : dsv;
 
     asset.type = "js";
     asset.setCode(`
-      import { ${parser}, autoType } from "d3-dsv";
+      import { ${parser} ${autoType} } from "d3-dsv";
       export default ${data};
     `);
     return [asset];
