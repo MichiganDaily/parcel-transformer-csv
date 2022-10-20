43adb61779e3cc9bb9808859dc03d08edef28ff2
erxclau <ericlau41802@gmail.com>
2022-10-20 18:44:15 -0400

    Import specific columns

--- src/index.js
+++ src/index.js
@@ -21,25 +21,52 @@ const getParser = (query, type) => {
   } else if (type === "tsv") {
     parser = tsv;
   }
-
   return parser;
 };
 
 module.exports = new Transformer({
   async transform({ asset }) {
-    const code = await asset.getCode();
+    let imports = "";
+
     const q = asset.query;
 
-    const shouldAutoType = !(q.has(auto) && q.get(auto) === "false");
-    const autoType = shouldAutoType ? `, ${auto}` : "";
+    const parser = getParser(q, asset.type);
+    if (parser === csv || parser === tsv) {
+      imports += parser;
+    } else {
+      imports += dsv;
+    }
 
-    const p = getParser(q, asset.type);
-    const data = `${p}(\`${code}\` ${autoType})`;
-    const parser = p === csv || p === tsv ? p : dsv;
+    const code = await asset.getCode();
+    let data = `${parser}(\`${code}\`)`;
+
+    const shouldAutoType = !(q.has(auto) && q.get(auto) === "false");
+    if (shouldAutoType) {
+      imports += "," + auto;
+    }
+
+    if (shouldAutoType) {
+      data = `${parser}(\`${code}\`, ${auto})`;
+    }
+
+    if (q.has("columns")) {
+      const columns = q.get("columns");
+      const mapper = `(d) => {
+        const columns = ${parser}(\`${columns}\`).columns;
+        if (columns === undefined) {
+          console.warn("The specified columns for ${asset.filePath} are undefined");
+          return d;
+        }
+        let m = d.map(r => Object.fromEntries(columns.map(c => [c, r[c]])));
+        m.columns = columns;
+        return m;
+      }`;
+      data = `(${mapper})(${data})`;
+    }
 
     asset.type = "js";
     asset.setCode(`
-      import { ${parser} ${autoType} } from "d3-dsv";
+      import { ${imports} } from "d3-dsv";
       export default ${data};
     `);
     return [asset];
